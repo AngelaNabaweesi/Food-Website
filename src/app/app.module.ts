31d877259652a38368197c543c6b4a4981a35247
Angela <nabaweesi3579@gmail.com>
2023-04-29 18:24:02 +0300

    Feat: MenuPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { MenuComponent } from './pages/menu/menu.component';
+import { MenupageComponent } from './pages/menupage/menupage.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MenuComponent } from './pages/menu/menu.component';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    MenuComponent
+    MenuComponent,
+    MenupageComponent
   ],
   imports: [
     BrowserModule,
